7868d870383918292b3b0d24a0d6fd8633da63f0
George Brown <gbrown128@ymail.com>
2017-01-30 21:40:42 +0000

    + Added function to get bookings for a particular day. - Removed message formatting and changed the return data from the booking request. This permits more customisation in the client.

--- libcal.py
+++ libcal.py
@@ -1,5 +1,3 @@
-7#! /usr/bin/env python3
-
 import datetime
 from urllib.parse import urlencode
 import requests
@@ -7,7 +5,7 @@ from bs4 import BeautifulSoup
 import re
 
 # Local settings file.
-import settings
+import libcal_settings as settings
 
 def write_success_message(booking_link, booking):
     # Regex magic pulls out details about the booking made.
@@ -76,7 +74,7 @@ def make_booking(time):
             break
 
     if(booking_sid == -1):
-        return 1
+        return(-1)
 
     # Prepare the form data for the booking.
     booking_payload = {
@@ -101,22 +99,82 @@ def make_booking(time):
         headers={'Referer': settings.referrer_url}
     )
 
+    return_data = {}
+    return_data['response'] = booking.content
+    return_data['info'] = booking_link
+
+    return(return_data)
+
     if booking.content == "0":
         # Success
         write_success_message(booking_link, booking)
     else:
         write_failure_message(booking_link, booking)
 
-def get_caldata(date):
+def get_bookings(date):
     # Payload for calendar data request.
     caldata = {
+        "m": "showNick",
+        "gid": settings.gid,
+        "d": date.strftime("%Y-%m-%d")
+    }
+
+    # Request the calendar data from the website.
+    request_url = settings.process_url + "?" + urlencode(caldata)
+
+    # Verify the web request went ok.
+    try:
+        bookings_src = requests.get(request_url)
+    except:
+        print("Page request failure: " + request_url)
+        return(-1)
+
+    if bookings_src.status_code != 200:
+        print(
+             "Page request failure: "
+             + str(bookings_src.status_code)
+             + " requesting: "
+             + request_url
+        )
+        return(-1)
+
+    # Parse the HTML out to make it more manageable.
+    soup = BeautifulSoup(bookings_src.content, 'html.parser')
+
+    # From the calendar table, return the first free session (first link)
+    bookings_src = soup.find('div', id='nick_box_inner')
+
+    bookings = []
+
+    for booking_src in bookings_src.find_all('tr'):
+        booking = {}
+        if booking_src.th:
+            continue
+        booking['room'] = re.search(
+            ".*Room (.+) \(",
+            booking_src.find('div', class_='tooltip').strong.decode()
+        ).group(1)
+        bookint['nick'] = booking_src.find('div', class_='nick_cont').string
+        times = re.search(
+            "(\d\d:\d\d).*(\d\d:\d\d)",
+            booking_src.find('td', class_='conf_time').decode()
+        )
+        session['start'] = times.group(1)
+        session['end'] = times.group(2)
+        bookings.append(booking)
+
+    return(bookings)
+
+def get_caldata(date):
+    # Payload for calendar data request.
+    payload = {
         "m": "calscroll",
         "gid": settings.gid,
         "date": date.strftime("%Y-%m-%d")
     }
 
     # Request the calendar data from the website.
-    request_url = settings.process_url + "?" + urlencode(caldata)
+    request_url = settings.process_url + "?" + urlencode(payload)
 
     # Verify the web request went ok.
     try:
@@ -146,6 +204,9 @@ def get_epoch(caldata):
     # From the calendar table, return the first free session (first link)
     first_free = caldata.a
 
+    if first_free == None:
+        return(-1)
+
     # Pick out the room name and the start time of the session.
     # It's likley these will require tuning for your instiution!
     room_name = re.search('Room (.*) \(', first_free['onclick']).group(1)
@@ -163,10 +224,3 @@ def get_epoch(caldata):
     # Compute the epoch for the very start of the day.
     day_epoch = first_sid - (23*epoch_room_idx+int(session_start))
     return(day_epoch)
-
-def main():
-    time = datetime.datetime(2017,2,10,16)
-    make_booking(time)
-
-if __name__ == "__main__":
-    main()
